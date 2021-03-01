9e8469be18d927d464ac34bc81a3e819aae6542f
Pranalibit <pranalipardeshi30@gmail.com>
2021-03-01 09:08:48 +0530

    Added Palm Project

---
+++ lib/utils/constant.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+int primaryColorCode = 0xFF00A5FF;
+
+// Color scheme
+const Color primaryColor = Color(0xFF00A5FF);
+const Color secondaryColor = Color(0xFF45D1FD);
+
+// Constant padding
+const double paddingS = 8.0;
+const double paddingM = 16.0;
+const double paddingL = 32.0;
+
+// Constant spacing
+const double spaceS = 8.0;
+const double spaceM = 16.0;
+
+// Constant text sizes
+const double headline1 = 96.0;
+const double headline2 = 60.0;
+const double headline3 = 48.0;
+const double headline4 = 34.0;
+const double headline5 = 24.0;
+const double headline6 = 20.0;
+const double subtitle1 = 16.0;
+const double subtitle2 = 14.0;
+const double body1 = 16.0;
+const double body2 = 14.0;
+const double button = 14.0;
+const double caption = 12.0;
+const double overline = 10.0;
+
+// Image Paths
+const String logo = "assets/images/logo.png";
+const String demo = "assets/images/demo.png";
+
+// Extras Paths
+const String loginIllustration = "assets/extras/demo.png";
+const String signupIllustration = "assets/extras/demo.png";
+
+// Links
+const BackgroundVideo = "https://youtu.be/Jp0nQJXmHc0";
+const MapTheme =
+    "https://api.maptiler.com/maps/positron/?key=xxxxxxxxxxxxxxxxx";
